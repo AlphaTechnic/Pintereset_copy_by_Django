56a4ddcd34b524f3e860108ace2cf14afd3fef9b
AlphaTechnic <alphatechnicist@gmail.com>
2021-07-04 23:29:29 +0900

    Make the secret key invisible

--- pragmatic/pragmatic/settings.py
+++ pragmatic/pragmatic/settings.py
@@ -12,15 +12,31 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os, environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# False if not in os.environ
+DEBUG = env('DEBUG')
+
+# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# reading .env file
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n$lxbh=)f^q$id=ut5d0pnex-hlc%zol)3-3j^gln#r3a5sh6#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
