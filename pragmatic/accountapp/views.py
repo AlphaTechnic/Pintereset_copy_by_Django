3d68a3ada15e58b209d0be9c6949b34ab757d0bd
AlphaTechnic <alphatechnicist@gmail.com>
2021-07-05 00:09:40 +0900

    Apply a base.html

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -5,4 +5,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse("안녕하세요")
+    # return HttpResponse("안녕하세요")
+
+    return render(request, 'base.html')
