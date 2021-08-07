73e7fcb7e60bf038fa070275f319559ae0b4e747
AlphaTechnic <alphatechnicist@gmail.com>
2021-08-08 00:16:39 +0900

    Add bootstrap link and google fonts link

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -7,4 +7,5 @@ from django.shortcuts import render
 def hello_world(request):
     # return HttpResponse("안녕하세요")
 
+    # 아래와 같은 가독성을 위해 accountapp/templates/accountapp과 같은 폴더 구조를 만들었다.
     return render(request, 'accountapp/hello_world.html')
